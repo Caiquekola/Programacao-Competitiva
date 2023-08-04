959aa7ef26ec530eb72f3a4c69f963dff02f5aea
Caique  Augusto <99914098+Caiquekola@users.noreply.github.com>
2023-08-03 21:36:22 -0300

    Add files via upload

---
+++ C++/URI/1114.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int r=0,N=2002;
+    while(r!=2002){
+        scanf("%d",&r);
+        if(r!=2002){
+            printf("Senha Invalida\n");
+        }else{
+            printf("Acesso Permitido\n");
+        }
+    }
+}
