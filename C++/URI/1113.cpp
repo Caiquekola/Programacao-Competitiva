1d83727f04aa9f03b24a8068cca5633d1ff53c1b
Caique  Augusto <99914098+Caiquekola@users.noreply.github.com>
2023-08-03 22:13:55 -0300

    Add files via upload

---
+++ C++/URI/1113.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x=0,y=-1;
+    while(x!=y){
+        
+        scanf("%d %d",&x,&y);
+        if(x<y){
+            printf("Crescente\n");
+        }else if(x>y){
+            printf("Decrescente\n");
+        }
+    }
+}
