3530893d3767fb5ccb2c9f4fc272217b9ceb846f
caique augusto <caique.augusto@unsoft.com.br>
2026-03-15 17:03:08 -0300

    maratonamineiravemai

--- LeetCode/LeetCode8.java
+++ LeetCode/LeetCode8.java
@@ -1,34 +1,49 @@
+import java.math.BigInteger;
+
 public class LeetCode8 {
     public static void main(String[] args) {
-        // System.out.println(Solution.myAtoi(args[0]));
-        for (int i = 1; i < 255; i++) {
-            System.out.println("Char: "+(char)i+""+"Number:"+i);
-        }
+        System.out.println(Solution.myAtoi(args[0]));
     }
 
     class Solution {
         public static int myAtoi(String s) {
             s = s.trim();
             char[] letras = s.toCharArray();
-            int pos=0;
-            if(letras[0]=='-'||letras[0]=='+'){
-                pos++;
-            }
-            if(letras.length==0 || (letras[0]=='-'&&letras.length==1)){
+            int pos = 0;
+            if (letras.length == 0) {
                 return 0;
             }
-            for(int i = pos; i < letras.length;i++){
-                if(((int)letras[i])>=48&&(int)letras[i]<=57){
-                    
+            if (letras[0] == '-' || letras[0] == '+') {
+                pos++;
+                if(letras.length==1){
+                    return 0;
                 }
-
             }
-            
-
-            
-
-
-            return 1001;
+            for (int i = pos; i < letras.length; i++) {
+                if (((int) letras[i]) >= 48 && (int) letras[i] <= 57) {
+                    pos++;
+                }else{
+                    break;
+                }
+            }
+            if ((letras[0] == '-' || letras[0] == '+')&&pos==1) {
+                {
+                    return 0;
+                }
+            }
+            if(pos == 0){
+                return 0;
+            }
+            BigInteger value = new BigInteger(s.substring(0,pos));
+            int comp = value.compareTo(new BigInteger(String.valueOf(Integer.MAX_VALUE)));
+            if(comp==1){
+                return Integer.MAX_VALUE;
+            }else if(comp==-1){
+                return Integer.MIN_VALUE;
+            }else {
+                return Integer.valueOf(s.substring(0, pos));
+            }
         }
     }
+
 }
---
+++ SQL/exc4.sql
@@ -0,0 +1,3 @@
+SELECT job MIN(SAL) as menorSalario, 
+FROM emp 
+GROUP BY job;
